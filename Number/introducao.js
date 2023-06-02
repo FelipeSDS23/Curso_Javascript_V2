54d5ca6f5ceb962f4ceabbfc454ffdee4e1e7f50
Felipe Santos <felipe03san@gmail.com>
2023-06-02 09:01:31 -0300

    Aula number concluida

---
+++ Number/introducao.js
@@ -0,0 +1,17 @@
+let numero = 1234567.890
+
+// console.log(numero.toFixed(2))
+// console.log(numero.toPrecision(17))
+// console.log(numero.toExponential())
+// console.log(numero.toString(16))
+
+// console.log(numero.toLocaleString("pt-BR"))
+// console.log(numero.toLocaleString("pt-BR", {style: "currency", currency: "BRL"}))
+// console.log(numero.toLocaleString("pt-PT", {style: "currency", currency: "EUR"}))
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+
+// let numString = "736.23";
+// console.log(isNaN(numString))
+// console.log(parseFloat(numString))
