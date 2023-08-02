3af718996388fbd314a9f8da555a840f23002edc
FelipeSDS23 <felipe03san@gmail.com>
2023-08-02 12:57:38 -0300

    ts 3

---
+++ typescript/types/src/alias.ts
@@ -0,0 +1,57 @@
+type testeAlias = number | string | boolean
+
+let teste3: testeAlias
+teste3 = 10
+teste3 = "teste 3"
+teste3 = true
+
+let teste4: testeAlias
+teste4 = 10 
+
+
+
+
+type user = {
+    nome: string,
+    idade?: number
+}
+
+const daniel: user = {
+    nome: "daniel"
+}
+
+const maria: user = {
+    nome: "maria",
+    idade: 30
+}
+
+
+
+
+
+
+
+type Sizes = "12px" | "18px" | "24px"
+
+const small: Sizes = "12px"
+const normal: Sizes = "18px"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default 1 
